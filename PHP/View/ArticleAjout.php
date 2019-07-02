7affb36ca33c4cf575a33c88320e191d6fa8d93a
Sephyres <sephyres@live.fr>
2019-07-02 15:06:35 +0200

    Exemple

--- PHP/View/ArticleAjout.php
+++ PHP/View/ArticleAjout.php
@@ -3,4 +3,5 @@ include "header.php";
 $p = new Articles(["titre"=>$_POST["titre"],"contenu"=>$_POST["contenu"],"date"=>$_POST["NOW()"]]);
 ArticlesManager::add($p);
 header ("location:Liste.php");
+
 ?>
--- PHP/View/ArticlesAjout.php
+++
@@ -1,6 +0,0 @@
-﻿<?php
-include "header.php";
-$p = new Articles(["titre"=>$_POST["titre"],"contenu"=>$_POST["contenu"]]);
-ArticlesManager::add($p);
-header ("location:Liste.php");
-?>
