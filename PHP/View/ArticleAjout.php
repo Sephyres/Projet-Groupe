100f403602a3c835612c34756ce651f1accefead
JulianKrf <50479166+JulianKrf@users.noreply.github.com>
2019-07-02 15:02:32 +0200

    Add files via upload
    Formulaire article + action

---
+++ PHP/View/ArticleAjout.php
@@ -0,0 +1,6 @@
+﻿<?php
+include "header.php";
+$p = new Articles(["titre"=>$_POST["titre"],"contenu"=>$_POST["contenu"],"date"=>$_POST["NOW()"]]);
+ArticlesManager::add($p);
+header ("location:Liste.php");
+?>
