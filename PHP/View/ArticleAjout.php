1a92226430967749c4c98f33b232a112fa8a7e5a
Sephyres <sephyres@live.fr>
2019-07-03 12:20:59 +0200

    Fix Articles

--- PHP/View/ArticleAjout.php
+++ PHP/View/ArticleAjout.php
@@ -1,37 +1,31 @@
 ﻿<?php
+if (isset($_POST["date"]))
+    $date = $_POST["date"];
+    else {
+    $date = "NOW()";
+    }
 
 switch ($_GET["action"]) {
 
     case 'ajouterArticle':
-        if (isset($edit))
-            $date = $edit->getDate();
-        else {
-            $date = "NOW()";
-            echo "Nope";
-        }
         $p = new Articles(["titre" => $_POST["titre"], "contenu" => $_POST["contenu"], "dateArticle" => $date]);
         ArticlesManager::add($p);
         break;
 
-    case "modifierArticle": {
-            if (isset($edit))
-                $date = $edit->getDate();
-            else {
-                $date = "NOW()";
-                echo "Nope";
+    case "modifierArticle":
+        $p = new Articles(["idArticle" => $_POST["idArticle"], ["titre" => $_POST["titre"], "contenu" => $_POST["contenu"], "dateArticle" => $date]]);
+        ArticlesManager::update($p);
+        break;
 
-                $p = new Articles(["idArticle" => $_POST["idArticle"], ["titre" => $_POST["titre"], "contenu" => $_POST["contenu"], "dateArticle" => $date]]);
-                ArticlesManager::update($p);
-                break;
-            }
-        }
 
-    case "supprimerArticle": {
-            $p = new Articles(["idArticle" => $_POST["idArticle"], ["titre" => $_POST["titre"], "contenu" => $_POST["contenu"], "dateArticle" => $date]]);
-            ArticlesManager::delete($p);
-            break;
-        }
+
+    case "supprimerArticle":
+        $p = ArticlesManager::getById($_POST["idArticle"]);
+        ArticlesManager::delete($p);
+        break;
 }
+
+
 
 /* 
 if (isset($edit))
--- PHP/View/FormArticle.php
+++ PHP/View/FormArticle.php
@@ -1,5 +1,8 @@
 <?php
 
+if (isset($GET["idArticle"]))
+$edit = ArticlesManager::getById($GET["idArticle"]);
+
 switch ($_GET["action"]) {
 	case 'ajouterArticle':
 		echo '<div id="DivSousTitre">
@@ -27,12 +30,14 @@ switch ($_GET["action"]) {
 		<h5>Modifier article</h5>
 	</div>
 	
-	<form id="formulaire" method="post" action="index.php?action=ajtArticle">
+	<form id="formulaire" method="post" action="index.php?action=modifArticle">
 		<!-- Quand le formulaire sera soumit par clic sur le bouton, les informations qu il contient seront stockées dans la variable $_POST, parce que la methode post a été sélectionnée -->
+		<input type="hidden" name="idArticle" value="' . $edit->getIdArticle() . '">
+		<input type="hidden" name="date" value="' . $edit->getDateArticle() . '">
 		Titre :<br>
-		<input type="text" name="titre"><br><br>
+		<input type="text" name="titre" value="' . $edit->getTitre() . '"><br><br>
 		Contenu : <br>
-		<textarea cols="150" rows="75" name="contenu" <?php if (isset($_POST["contenu"])) echo "value= " .$_POST["contenu"] ;?></textarea><br><br>
+		<textarea cols="150" rows="75" name="contenu" value="' . $edit->getContenu() . '"></textarea><br><br>
 	
 	
 		<input type="submit" value="valider" />
@@ -43,17 +48,9 @@ switch ($_GET["action"]) {
 		break;
 	
 		case 'supprimerArticle':
-		echo '<div id="DivSousTitre">
-		<h5>Supprimer Article</h5>
-	</div>
-	
-	<form id="formulaire" method="post" action="index.php?action=ajtArticle">
-		<!-- Quand le formulaire sera soumit par clic sur le bouton, les informations qu il contient seront stockées dans la variable $_POST, parce que la methode post a été sélectionnée -->
-		Titre :<br>
-		<input type="text" name="titre"><br><br>
-		Contenu : <br>
-		<textarea cols="150" rows="75" name="contenu" <?php if (isset($_POST["contenu"])) echo "value= " .$_POST["contenu"] ;?></textarea><br><br>
-	
+		echo 'Êtes vous sûr de bien vouloir supprimer l\'article suivant : ' . $edit->getTitre() . '
+		<form id="formulaire" method="post" action="index.php?action=supprArticle">
+		<input type="hidden" name="idArticle" value="' . $edit->getIdArticle() . '">
 	
 		<input type="submit" value="valider" />
 		<input type="reset" value="annuler" />
