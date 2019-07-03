4a81de37a7e44566c7ae30c63ab5ddc77e6906d7
JulianKrf <50479166+JulianKrf@users.noreply.github.com>
2019-07-03 11:27:18 +0200

    Modifier/supprimer sur formulaire article
    Mise a jour avec update/delete à verifier

--- PHP/View/ArticleAjout.php
+++ PHP/View/ArticleAjout.php
@@ -1,6 +1,59 @@
 ﻿<?php
-include "header.php";
-$p = new Articles(["titre"=>$_POST["titre"],"contenu"=>$_POST["contenu"],"date"=>$_POST["NOW()"]]);
+
+switch ($_GET["action"]) {
+
+    case 'ajouterArticle':
+        if (isset($edit))
+            $date = $edit->getDate();
+        else {
+            $date = "NOW()";
+            echo "Nope";
+        }
+        $p = new Articles(["titre" => $_POST["titre"], "contenu" => $_POST["contenu"], "dateArticle" => $date]);
+        ArticlesManager::add($p);
+        break;
+
+    case "modifierArticle": {
+            if (isset($edit))
+                $date = $edit->getDate();
+            else {
+                $date = "NOW()";
+                echo "Nope";
+
+                $p = new Articles(["idArticle" => $_POST["idArticle"], ["titre" => $_POST["titre"], "contenu" => $_POST["contenu"], "dateArticle" => $date]]);
+                ArticlesManager::update($p);
+                break;
+            }
+        }
+
+    case "supprimerArticle": {
+            $p = new Articles(["idArticle" => $_POST["idArticle"], ["titre" => $_POST["titre"], "contenu" => $_POST["contenu"], "dateArticle" => $date]]);
+            ArticlesManager::delete($p);
+            break;
+        }
+}
+
+/* 
+if (isset($edit))
+    $date = $edit->getDate();
+else {
+    $date = "NOW()";
+    echo "Nope";
+}
+$p = new Articles(["titre" => $_POST["titre"], "contenu" => $_POST["contenu"], "dateArticle" => $date]);
 ArticlesManager::add($p);
-header ("location:Liste.php");
-?>
+
+case "modifierArticle":
+    {
+        $p = new Articles(["id"=>$_POST["id"],["titre" => $_POST["titre"], "contenu" => $_POST["contenu"], "dateArticle" => $date]]);
+        ArticlesManager::update($p);
+        break;
+    }
+
+    case "supprimmerArticle":
+    {
+        $p = new Articles(["id"=>$_POST["id"],["titre" => $_POST["titre"], "contenu" => $_POST["contenu"], "dateArticle" => $date]]);
+        ArticlesManager::delete($p);
+    }
+
+//header("location:Index.php"); */
