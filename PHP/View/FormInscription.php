4256daf60656684085038282a07be117e6c5f0bc
Sephyres <sephyres@live.fr>
2019-07-03 11:28:31 +0200

    Lissage CSS

--- PHP/View/FormInscription.php
+++ PHP/View/FormInscription.php
@@ -1,18 +1,32 @@
-<form method="post" action="<?php echo serverRoot; ?>?action=inscript">
-    <label for="nom">Nom :</label>
-    <input type="text" name="nom" id="nom" autofocus required pattern="^[A-Za-z]+$">
-    <label for="prenom">Prénom :</label>
-    <input type="text" name="prenom" id="prenom" required pattern="^[A-Za-z]+$">
-    <label for="pseudo">Pseudo :</label>
-    <input type="text" name="pseudo" id="pseudo" required pattern="^[A-Za-z0-9]+$">
-    <label for="mail">Email :</label>
-    <input type="text" name="mail" id="mail" required pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?">
-    <label for="mdp">Mot de passe :</label>
-    <input type="password" name="mdp" id="mdp" required pattern="(?=.*\d)(?=.*[@ç_])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z@ç_]{8,30}">
-    <label for="confirm">Confirmez le de passe :</label>
-    <input type="password" name="confirm" id="confirm" required pattern="(?=.*\d)(?=.*[@ç_])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z@ç_]{8,30}">
-
-    <input type="submit" value="Valider">
-    <input type="reset" value="Annuler">
-</form>
-
+<div id="formSignIn">
+    <form method="post" action="<?php echo serverRoot; ?>?action=inscript">
+        <div class="formSignInLine">
+            <div class="formSignInCase"><label for="nom">Nom :</label></div>
+            <div class="formSignInCase"><input type="text" name="nom" id="nom" autofocus required pattern="^[A-Za-z]+$"></div>
+        </div>
+        <div class="formSignInLine">
+            <div class="formSignInCase"><label for="prenom">Prénom :</label></div>
+            <div class="formSignInCase"><input type="text" name="prenom" id="prenom" required pattern="^[A-Za-z]+$"></div>
+        </div>
+        <div class="formSignInLine">
+            <div class="formSignInCase"><label for="pseudo">Pseudo :</label></div>
+            <div class="formSignInCase"><input type="text" name="pseudo" id="pseudo" required pattern="^[A-Za-z0-9]+$"></div>
+        </div>
+        <div class="formSignInLine">
+            <div class="formSignInCase"><label for="mail">Email :</label></div>
+            <div class="formSignInCase"><input type="text" name="mail" id="mail" required pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"></div>
+        </div>
+        <div class="formSignInLine">
+            <div class="formSignInCase"><label for="mdp">Mot de passe :</label></div>
+            <div class="formSignInCase"><input type="password" name="mdp" id="mdp" required pattern="(?=.*\d)(?=.*[@ç_])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z@ç_]{8,30}"></div>
+        </div>
+        <div class="formSignInLine">
+            <div class="formSignInCase"><label for="confirm">Confirmez le Mot de passe :</label></div>
+            <div class="formSignInCase"><input type="password" name="confirm" id="confirm" required pattern="(?=.*\d)(?=.*[@ç_])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z@ç_]{8,30}"></div>
+        </div>
+        <div class="formSignInLine">
+            <input type="submit" value="Valider">
+            <input type="reset" value="Annuler">
+        </div>
+    </form>
+</div>
